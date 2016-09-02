02a8afd186ba385483915dca4285559646d3843b
Justin Staubach <justin@staubach.us>
2016-09-02 15:36:28 -0400

    some files

---
+++ js/ng-wap-window/ngWapWindow.js
@@ -0,0 +1,8 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/screenfull.js/3.0.2/screenfull.min.js></script>
+
+
+function requestFullScreen() {
+	if (screenfull.enabled) {
+        screenfull.request();
+    }
+}
